430cca241899efce73140e1e28b5553c6b00d44a
EyadAymanM <111561121+EyadAymanM@users.noreply.github.com>
2025-01-25 16:27:07 +0200

    intialized prisma & added movies-api module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MoviesApiModule } from './movies-api/movies-api.module';
 
 @Module({
-  imports: [],
+  imports: [MoviesApiModule],
   controllers: [AppController],
   providers: [AppService],
 })
