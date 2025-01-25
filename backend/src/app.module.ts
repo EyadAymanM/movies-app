555122766fb83b1d67c8054aa50286d02a4aedc1
EyadAymanM <111561121+EyadAymanM@users.noreply.github.com>
2025-01-25 20:29:16 +0200

    created blank resources for favourites & update README.md

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { MoviesApiModule } from './movies-api/movies-api.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { FavouriteModule } from './favourite/favourite.module';
 
 @Module({
-  imports: [MoviesApiModule, PrismaModule],
+  imports: [MoviesApiModule, PrismaModule, FavouriteModule],
   controllers: [],
   providers: [],
 })
